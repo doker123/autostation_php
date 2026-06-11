786bd677433b7743d0e8b5f4a20ddac874086f47
Doker123 <dodo2larin@gmail.com>
2026-06-11 08:36:32 +0700

    Поправил удаление записей из системы.

--- config/connectionDb.php
+++ config/connectionDb.php
@@ -3,13 +3,12 @@ class Database
 {
     private static ?\PDO $instance = null;
     private static array $config = [
-        'host' => 'mysqldb',
-//      'host' => 'localhost',
-        'dbname' => 's3r4duex_m1',
-        'username' => 's3r4duex',
-        'password' => 'oyyqwF02',
-        'charset' => 'utf8mb4',
-
+        // 'host' => 'mysqldb',
+        "host" => "localhost",
+        "dbname" => "s3r4duex_m1",
+        "username" => "s3r4duex",
+        "password" => "oyyqwF02",
+        "charset" => "utf8mb4",
     ];
     public static function getInstance(): \PDO
     {
@@ -21,16 +20,21 @@ class Database
     private static function createConnection(): \PDO
     {
         $dsn = sprintf(
-            'mysql:host=%s;dbname=%s;charset=%s',
-            self::$config['host'],
-            self::$config['dbname'],
-            self::$config['charset']
+            "mysql:host=%s;dbname=%s;charset=%s",
+            self::$config["host"],
+            self::$config["dbname"],
+            self::$config["charset"],
         );
         $options = [
-            PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-            PDO::ATTR_EMULATE_PREPARES   => false,
+            PDO::ATTR_EMULATE_PREPARES => false,
         ];
-        return new \PDO($dsn, self::$config['username'], self::$config['password'], $options);
+        return new \PDO(
+            $dsn,
+            self::$config["username"],
+            self::$config["password"],
+            $options,
+        );
     }
 }
--- delete.php
+++ delete.php
@@ -1,22 +1,5 @@
-<?php ob_start();
-require_once "config/helpers.php";?>
-<!doctype html>
-<html lang="ru">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Автостоянка</title>
-    <link rel="stylesheet" href="<?= asset('css/style.css') ?>">
-    <script defer src="<?= asset('scripts/script.js') ?>"></script>
-</head>
-<body>
-<header class="header-menu">
-    <a href="<?= url(' ')?>"><h1>Автостоянка</h1></a>
-</header>
-<?php;
-require_once "config/connectionDb.php";
+<?php
+require_once __DIR__ . "/config/connectionDb.php";
 ini_set("display_errors", 1);
 ini_set("display_startup_errors", 1);
 error_reporting(E_ALL);
@@ -77,7 +60,3 @@ try {
 header("Location: " . PROJECT_PATH . "/home");
 exit();
 ?>
-</main>
-</body>
-</html>
-
